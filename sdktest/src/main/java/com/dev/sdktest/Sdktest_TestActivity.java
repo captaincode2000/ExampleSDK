5a965cc211533ccaf89ac2741dc04e9e893fbe00
aldairmg <a2017@MacBook-Pro-de-2017.local>
2019-09-09 16:48:24 -0500

    Initial commit

---
+++ sdktest/src/main/java/com/dev/sdktest/Sdktest_TestActivity.java
@@ -0,0 +1,23 @@
+package com.dev.sdktest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.zeus.zcommon.ZCommonActivityTest;
+
+public class Sdktest_TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sdktest_activity_test);
+    }
+
+
+    public void onClickStart(View view) {
+        Intent intent = new Intent(Sdktest_TestActivity.this, ZCommonActivityTest.class);
+        startActivity(intent);
+    }
+}
